40db0dcc10a093eb12fbc398d3a9c78810145098
Zie Sturges <zie@sturges.com.au>
2025-04-10 14:55:12 +1000

    2015/one: part oen solved

--- 2015/one/src/main.rs
+++ 2015/one/src/main.rs
@@ -1,3 +1,31 @@
+use std::{fs, time::Instant};
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+
+    let input = match fs::read_to_string("input") {
+        Ok(input) => input,
+        Err(e) => {
+            panic!("Failed to open file: {e}")
+        }
+    };
+
+    let mut floor: i32 = 0;
+
+    for char in input.chars() {
+        floor += up_or_down(char);
+    }
+
+    println!("The correct floor is: {floor}");
+    let end = Instant::now();
+    println!("Took {} ns", (end - start).as_nanos());
+}
+
+fn up_or_down(char: char) -> i32 {
+    match char {
+        '(' => 1,
+        ')' => -1,
+        '\n' => 0,
+        _ => panic!("Invalid input: {char}, input should either be '(' or ')'"),
+    }
 }
